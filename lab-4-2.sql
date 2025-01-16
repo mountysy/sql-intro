7125b77e63a6bd6a8bcf2cc8cc475d5bf4130430
Shan Yan <shan.yan@kellogg.northwestern.edu>
2025-01-16 22:47:05 +0000

    Complete Lab 4

--- lab-4-2.sql
+++ lab-4-2.sql
@@ -1,8 +1,11 @@
 -- What are the first and last names of the players who 
 -- played for the 2020 Chicago Cubs?
 
-SELECT players.first_name, player.last_name
-FROM 
+SELECT players.first_name, players.last_name
+FROM players INNER JOIN stats ON players.id = stats.player_id INNER JOIN teams ON teams.id = stats.team_id
+WHERE teams.name = "Chicago Cubs"
+AND teams.year = 2020;
+
 
 -- Hint: combine WHERE clauses using AND, e.g.
 -- WHERE column1 = expression1
--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,5 +1,11 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
 
+SELECT teams.name, players.first_name, players.last_name, stats.home_runs
+FROM stats INNER JOIN players ON stats.player_id = players.id INNER JOIN teams ON stats.team_id = teams.id
+WHERE teams.year = 2019
+ORDER BY home_runs DESC
+LIMIT 1;
+
 -- Expected result:
 --
 -- +---------------+------------+-----------+-----------+
